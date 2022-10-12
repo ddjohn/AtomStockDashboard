e483cc3dfc6faaad90aa2eb46ab2ee0c05a29988
ddjohn <ddjohn@gmail.com>
2022-10-12 19:46:45 +0200

    Update stockdashboard.ino

--- stockdashboard.ino
+++ stockdashboard.ino
@@ -6,23 +6,27 @@
 
 #include "config.h"
 
+#define MATRIX_SIZE 5
+#define YAHOO_URL "https://query1.finance.yahoo.com/v8/finance/chart/"
+#define YAHOO_USERAGENT "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+
 String stocks[] = {
-  "ABB.ST",    "ALFA.ST",    "ASSA-B.ST",   "ATCO-A.ST", "AZN.ST", 
-  "BOL.ST",    "ERIC-B.ST",  "ESSITY-B.ST", "EVO.ST",    "HEXA-B.ST", 
-  "HM-B.ST",   "INVE-B.ST",  "NDA-SE.ST",   "SAND.ST",   "SCA-B.ST", 
-  "SEB-A.ST",  "SHB-A.ST",   "SINCH.ST",    "SKA-B.ST",  "SKF-B.ST", 
-  "SWED-A.ST", "SWMA.ST",    "TEL2-B.ST",   "TELIA.ST",  "VOLV-B.ST"
+  "ABB.ST"   , "ALFA.ST"  , "ASSA-B.ST"  , "ATCO-A.ST", "AZN.ST"   ,
+  "BOL.ST"   , "ERIC-B.ST", "ESSITY-B.ST", "EVO.ST"   , "HEXA-B.ST",
+  "HM-B.ST"  , "INVE-B.ST", "NDA-SE.ST"  , "SAND.ST"  , "SCA-B.ST" ,
+  "SEB-A.ST" , "SHB-A.ST" , "SINCH.ST"   , "SKA-B.ST" , "SKF-B.ST" ,
+  "SWED-A.ST", "SWMA.ST"  , "TEL2-B.ST"  , "TELIA.ST" , "VOLV-B.ST",
 };  
 
-uint8_t buffer[2 + 5 * 5 * 3];
+uint8_t buffer[2 + MATRIX_SIZE * MATRIX_SIZE * 3];
 HTTPClient http;
 
 void setup() {
   M5.begin(true, false, true);
   Serial.println("setup()");
 
-  buffer[0] = 5;
-  buffer[1] = 5;
+  buffer[0] = MATRIX_SIZE;
+  buffer[1] = MATRIX_SIZE;
 
   WiFi.begin(SSID, PASSWORD);  
 }
@@ -34,22 +38,21 @@ void setLed(int led, int red, int green, int blue) {
   M5.dis.displaybuff(buffer);    
 }
 
-
 void showOK() {
-  for(int i = 0; i < 25; i++) {
+  for(int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) {
     setLed(i, 0, 0, 0);
   }  
 }
 
 void showNOK() {
-   for(int i = 0; i < 25; i++) {
+   for(int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) {
     setLed(i, random(1, 128), random(1, 128), random(1, 128));
   }
 }
 
 float pct(String stock) {
-    http.begin("https://query1.finance.yahoo.com/v8/finance/chart/" + stock + "?interval=1d&range=1d");
-    http.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
+    http.begin(YAHOO_URL + stock + "?interval=1d&range=1d");
+    http.setUserAgent(YAHOO_USERAGENT);
     http.setReuse(true);
         
     Serial.println(http.GET());
@@ -86,19 +89,19 @@ void loop() {
 
     for (int i = 0; i < sizeof stocks/sizeof stocks[0]; i++) {
       float percentage = pct(stocks[i]);
-      /*if(percentage > 1) {
-        setLed(i, 0, 128, 128);
-      }
-      else*/ if(percentage > 0) {
-        setLed(i, 0, 128, 0);        
+      
+     if(percentage > 0) {
+        // Green
+        float value = 100.0 * percentage > 192 ? 192 : 100 * percentage;
+        setLed(i, 0, value, 0);        
       } 
-      /*else if(percentage < 1) {
-        setLed(i, 128, 0, 128);
-      }*/
       else if(percentage < 0) {
-        setLed(i, 128, 0, 0);
+        // Red              
+        float value = -100.0 * percentage > 192 ? 192 : - 100 * percentage;
+        setLed(i, value, 0, 0);
       }
       else {
+        // Blue
         setLed(i, 0, 0, 128);        
       }
       Serial.println(percentage);
